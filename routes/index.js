42894033609e806af421844ec1ef3b5a874bea97
justincoon <justin.tyler.coon@gmail.com>
2015-04-20 23:21:01 -0400

    cleaned up file structure

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
-var parse = require('../parser');
-var request = require('request').defaults({'jar':'true'});
 
 router.get('/', function(req, res, next) {
-	req.session.cart = {classes:'blah'};
 	res.render('index');
 });
-router.get('/a', function(req, res, next) {
-	console.log(req.session);
-	res.render('index');
+router.get('/:file', function(req, res, next) {
+	var filePath = path.join(__dirname, '../public/data/' + req.params.file);
+
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+	    if (!err){
+			res.write(data);
+			res.end();
+	    }else{
+		console.log(err);
+	    }
+	});
 });
 module.exports = router;
